813310a325fef88fac1201ad0a6549ffa2aa93a3
Anjoliekate <anjoliekatecummins@gmail.com>
2024-02-19 21:32:00 -0700

    MOdifying strategy

--- tradingbot.py
+++ tradingbot.py
@@ -1,47 +1,38 @@
-from lumibot.traders import Trader
-from lumibot.brokers import Alpaca
-from lumibot.backtesting import YahooDataBacktesting
-from lumibot.strategies.strategy import Strategy
-from datetime import datetime
-from dotenv import load_dotenv
-from alpaca_trade_api import REST
-from timedelta import Timedelta
-from finbert_utils import estimate_sentiment
-import os
-
 # lumibot is a trading library that allows you to backtest and trade stocks using a variety of strategies.
 # Alpaca is a brokerage that allows you to trade stocks using an API and provides a paper trading environment.
 # YahooDataBacktesting is a class that allows you to backtest a strategy using historical stock data from Yahoo Finance.
 # This strategy uses finbert_utils to estimate the sentiment of news articles and uses that to determine if it should buy or sell a stock.
 
-# load in my env variable
+from lumibot.strategies.strategy import Strategy
+from lumibot.backtesting import YahooDataBacktesting
+from datetime import datetime
+from timedelta import Timedelta
+from alpaca_trade_api import REST
+from finbert_utils import estimate_sentiment
+import os
+
+# Load in environment variables
+from dotenv import load_dotenv
 load_dotenv("info.env")
 
-# get env variables
+# Get environment variables
 API_KEY = os.getenv('API_KEY')
 API_SECRET = os.getenv('API_SECRET')
 BASE_URL = os.getenv('BASE_URL')
 
+# Alpaca credentials
 ALPACA_CREDS = {
     "API_KEY": API_KEY,
     "API_SECRET": API_SECRET,
     "PAPER": True
 }
 
-class MLTrader(Strategy):
-
-    def initialize(self, symbol:str="SPY", cash_at_risk:float=.5):
-        self.symbol = symbol
-        self.sleeptime = "24H"
-        self.last_trade = None
-        self.cash_at_risk = cash_at_risk
+class BotStrategy(Strategy):
+    
+    def initialize(self):
+        self.sleeptime = "1D"
         self.api = REST(base_url=BASE_URL, key_id=API_KEY, secret_key=API_SECRET)
-
-    def position_sizing(self):
-        cash = self.get_cash()
-        last_price = self.get_last_price(self.symbol)
-        quantity = round(cash * self.cash_at_risk / last_price, 0)
-        return cash, last_price, quantity
+        self.last_trade = None
 
     def get_dates(self):
         today = self.get_datetime()
@@ -50,34 +41,42 @@ class MLTrader(Strategy):
 
     def get_sentiment(self):
         today, three_days_prior = self.get_dates()
-        news = self.api.get_news(symbol = self.symbol, start=three_days_prior, end=today)
+        news = self.api.get_news(symbol=self.symbol, start=three_days_prior, end=today)
         news = [ev.__dict__["_raw"]["headline"] for ev in news]
-        probablility, sentiment = estimate_sentiment(news)
-        return probablility, sentiment
+        probability, sentiment = estimate_sentiment(news)
+        return probability, sentiment
 
     def on_trading_iteration(self):
-        cash, last_price, quantity = self.position_sizing()
+        symbol = "AMZN"  
+        last_price = self.get_last_price(symbol)
+        quantity = self.cash // last_price
+        self.symbol = symbol
         probability, sentiment = self.get_sentiment()
-        if cash > last_price:
-                if sentiment == "positive" and probability > .999:
-                    if self.last_trade == "sell":
-                         self.sell_all()
-                    order = self.create_order(self.symbol, quantity, "buy", type="bracket", take_profit_price=last_price*1.20, stop_loss_price=last_price*.95)
-                    self.submit_order(order)
-                    self.last_trade = "buy"
-                elif sentiment == "negative" and probability > .999:
-                    if self.last_trade == "buy":
-                         self.sell_all()
-                    order = self.create_order(self.symbol, quantity, "sell", type="bracket", take_profit_price=last_price*.8, stop_loss_price=last_price*1.05)
-                    self.submit_order(order)
-                    self.last_trade = "sell"
 
-start_date = datetime(2020, 1, 1)
-end_date = datetime(2024, 1, 31)
+        if self.cash > last_price:
+            # if the sentiment is positive and the probability for gain is high, buy the stock
+            if sentiment == "positive" and probability > 0.999:
+                if self.last_trade == "sell":
+                    self.sell_all()
+                order = self.create_order(symbol, quantity, "buy")
+                self.submit_order(order)
+                self.last_trade = "buy"
+            
+            #if the news indicates a negative sentiment and the probability for loss is high, sell the stock
+            elif sentiment == "negative" and probability > 0.999:
+                if self.last_trade == "buy":
+                    self.sell_all()
+                order = self.create_order(symbol, quantity, "sell")
+                self.submit_order(order)
+                self.last_trade = "sell"
 
-broker = Alpaca(ALPACA_CREDS)
+if __name__ == "__main__":
+    start_date = datetime(2022, 1, 1)
+    end_date = datetime(2024, 1, 31)
+    BotStrategy.backtest(
+        YahooDataBacktesting,
+        start_date,
+        end_date,
+        benchmark_asset="AMZN",
 
-strategy = MLTrader(name="MLTrader", broker=broker, parameters={"symbol": "SPY", "cash_at_risk": .5})
-strategy.backtest(
-    YahooDataBacktesting, start_date, end_date, parameters={}
-)
+    )
